b01f8bece5adbf681912819361d422622b615be3
nixon <xiaoyao_ning@163.com>
2016-06-14 17:19:53 +0800

    添加Project4

---
+++ project4/src/com/nixon/Test.java
@@ -0,0 +1,18 @@
+package com.nixon;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Hello");
+        arrayList.add("World");
+
+        System.out.println(arrayList.size());
+
+        String[] array = {"Hello", "World"};
+
+
+        System.out.println(array.length);
+    }
+}
