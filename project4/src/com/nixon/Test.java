d27364ba1428b61ccc76aeea54d526bb7024f798
nixon <xiaoyao_ning@163.com>
2016-06-14 20:25:30 +0800

    完成练习16

--- project4/src/com/nixon/Test.java
+++ project4/src/com/nixon/Test.java
@@ -1,5 +1,6 @@
 package com.nixon;
 
+import com.practice.P16;
 import java.util.*;
 
 public class Test {
@@ -7,12 +8,12 @@ public class Test {
         ArrayList<String> arrayList = new ArrayList<String>();
         arrayList.add("Hello");
         arrayList.add("World");
-
         System.out.println(arrayList.size());
 
         String[] array = {"Hello", "World"};
-
-
         System.out.println(array.length);
+
+        P16 p16 = new P16();
+        p16 = null;
     }
 }
---
+++ project4/src/com/practice/P16.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class P16 {
+    public P16() {
+
+        String[] array = new String[3];
+        array[0] = "YES";
+        array[1] = "Ah ha!";
+        array[2] = "Yoooooooo~";
+
+        for (String item : array) {
+            System.out.println(item);
+        }
+
+    }
+}
