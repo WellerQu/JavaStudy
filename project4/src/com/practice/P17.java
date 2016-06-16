2cfc2b26c6f5ec128bd0ac753c64ca09e109c776
邱巍 <qiuwei@qiuweideMacBook-Pro.local>
2016-06-16 10:02:23 +0800

    添加练习17

---
+++ project4/src/com/practice/P17.java
@@ -0,0 +1,14 @@
+package com.nixon.practice;
+
+public class P17 {
+    public P17() {
+
+    }
+
+    private String str;
+
+    public P17(String str) {
+        this.str = str;
+        System.out.println(str);
+    }
+}
