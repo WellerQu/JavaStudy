f68ddaa50d42a8da1a558c7713868ffcade10255
nixon <xiaoyao_ning@163.com>
2016-06-17 11:42:26 +0800

    完善SimpleEnum示例

--- project5/src/com/nixon/SimpleEnum.java
+++ project5/src/com/nixon/SimpleEnum.java
@@ -1,11 +1,15 @@
 package com.nixon;
 
+import com.practice.Spiciness;
+
 public class SimpleEnum {
     public SimpleEnum() {
 
     }
 
     public static void main(String[] args) {
+        Spiciness spiciness = Spiciness.HOT;
         System.out.println("Hello Enum");
+        System.out.println(spiciness);
     }
 }
