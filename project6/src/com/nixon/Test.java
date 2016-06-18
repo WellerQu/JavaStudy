a5be4da7d1d46b6086d1d5519ebf9f18fca24aee
nixon <xiaoyao_ning@163.com>
2016-06-18 11:37:56 +0800

    添加Project6

---
+++ project6/src/com/nixon/Test.java
@@ -0,0 +1,15 @@
+package com.nixon;
+
+public class Test {
+    public Test() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Project6");
+    }
+}
+
+class Helper {
+
+}
