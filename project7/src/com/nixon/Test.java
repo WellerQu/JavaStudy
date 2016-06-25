d8807b25139fa0a2cbfdef0460873f1246520138
nixon <xiaoyao.ning@gmail.com>
2016-06-25 15:02:47 +0800

    添加final用法

--- project7/src/com/nixon/Test.java
+++ project7/src/com/nixon/Test.java
@@ -1,8 +1,25 @@
 package com.nixon;
 
 public class Test {
+    private static final String str = "Hello";
+
     public static void main(String[] args) {
         Benz benz = new Benz();
+        benz.sayHello();
+
+        Car car = new Car("Car");
+        car.sayHello();
+
+        Car newCar = new Benz();
+        newCar.sayHello();
+
+        System.out.println("-------");
+
+        System.out.println(str);
+        // str = "123";  // 提示语法错误, final 数据不可再更改
+
+
+        QQ qq = new QQ();
     }
 }
 
@@ -11,6 +28,10 @@ class Car {
     Car(String name) {
         System.out.println("Hello Car: " + name);
     }
+
+    void sayHello() {
+        System.out.println("I'm Car");
+    }
 }
 
 class Benz extends Car {
@@ -18,4 +39,23 @@ class Benz extends Car {
         super("Benz");
         System.out.println("Hello Benz");
     }
+
+    @Override
+    void sayHello() {
+        System.out.println("I'm Benz");
+    }
 }
+
+final class QQ {
+    QQ() {
+        System.out.println("Hello QQ");
+    }
+}
+
+/* //: 提示语法错误, final类不可再继承
+class ZZ extends QQ {
+    ZZ() {
+         System.out.println("Hello ZZ");
+    }
+}
+//*/
