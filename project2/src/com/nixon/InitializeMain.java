98ba9a81499fa524ae283c8418dc19dc1a7dd379
nixon <xiaoyao.ning@gmail.com>
2016-08-10 14:09:02 +0800

    测试javacomplete2插件

--- project2/src/com/nixon/InitializeMain.java
+++ project2/src/com/nixon/InitializeMain.java
@@ -4,5 +4,8 @@ public class InitializeMain {
     public static void main(String[] args) {
         Animal animal = new Animal();
         Fish fish = new Fish();
+
+        Integer i = 0;
+        i.toString();
     }
 }
