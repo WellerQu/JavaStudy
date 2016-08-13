439a5094d52f3d99cd2112f8af0bf6b40143791d
nixon <xiaoyao.ning@gmail.com>
2016-08-13 14:23:25 +0800

    添加ArrayList并测试AddMissing功能

--- project8/src/polymorphism/music/Music.java
+++ project8/src/polymorphism/music/Music.java
@@ -1,9 +1,21 @@
 package polymorphism.music;
 
+import java.util.ArrayList;
+
 public class Music {
 
     public static void main(String[] args) {
         Instrument flute = new Wind();
         flute.play(Note.MIDDLE_C);
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+
+        for (Integer i : a) {
+            System.out.println(i);
+        }
     }
 }
